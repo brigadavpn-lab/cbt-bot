10f12ffe8ea811f424728913a4447bfbbab7551b
brigadavpn-lab <brigadavpn@gmail.com>
2026-06-15 18:06:04 +0000

    fix(db): restore revision variables in is_age_confirmed migration

--- alembic/versions/g2h3i4j5k6l7_add_is_age_confirmed_to_users.py
+++ alembic/versions/g2h3i4j5k6l7_add_is_age_confirmed_to_users.py
@@ -1,9 +1,10 @@
-"""add is_age_confirmed to users
-
-Revision ID: g2h3i4j5k6l7
-Revises: f1a2b3c4d5e6
-"""
+"""add is_age_confirmed to users"""
 from alembic import op
+
+revision = 'g2h3i4j5k6l7'
+down_revision = 'f1a2b3c4d5e6'
+branch_labels = None
+depends_on = None
 
 def upgrade() -> None:
     op.execute("""
